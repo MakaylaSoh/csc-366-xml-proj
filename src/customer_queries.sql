e775db5ce0c61541733777e597e9c6d45e154983
sameera <sameerabali5@gmail.com>
2024-03-10 16:18:11 -0700

    finished all customer queries

--- src/customer_queries.sql
+++ src/customer_queries.sql
@@ -16,7 +16,7 @@ WHERE ExtractValue(xml_doc, '/purchase/customer') IS NOT NULL AND LENGTH(Extract
 GROUP BY customerName, phoneNumber;
 
 
--- -- Customers are able to access all their previous order histories through their card number OR phone number if they have a rewards account.
+-- -- Customers are able to access all their previous order histories through their card number OR phone number if they have a rewards account. 
 SELECT 
     xml_purchase.id AS purchaseId,
     ExtractValue(xml_doc, '/purchase/order/order_date') AS DatePurchased, 
@@ -28,7 +28,16 @@ WHERE (ExtractValue(xml_doc, '/purchase/customer/card_number') = '*****3333') | 
 GROUP BY purchaseId;
 
 
--- Customers are able to modify the name on their rewards account. 
+-- Customers are able to have a running total of points earned till date with the purchases they've made. This only applies to customers with rewards accounts. 
+SELECT 
+    xml_purchase.id AS purchaseId,
+    ExtractValue(xml_doc, '/purchase/order/order_date') AS DatePurchased, 
+    ExtractValue(xml_doc, '/purchase/order/total_cost') AS OrderCost,
+    ExtractValue(xml_doc, '/purchase/customer/rewards_account/total_points') AS PointsPerPurchase,
+    SUM(ExtractValue(xml_doc, '/purchase/customer/rewards_account/total_points')) OVER (order by xml_purchase.id) as RunningTotalPoints
+FROM xml_purchase
+WHERE (ExtractValue(xml_doc, '/purchase/customer/card_number') = '*****3333') | (ExtractValue(xml_doc, '/purchase/customer/rewards_account/phone_number') = '(333)333-3333')
+GROUP BY purchaseId;
 
 
 
