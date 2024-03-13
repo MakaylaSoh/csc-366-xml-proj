d6f22d48757930b4731eb682d2c8471bc2c33187
sameera <sameerabali5@gmail.com>
2024-03-13 16:29:43 -0700

    customer xml and sql done

--- src/customer_sql.sql
+++ src/customer_sql.sql
@@ -1,39 +1,38 @@
--- For all customers who have rewards accounts with more than 10 points, return their first name, account number, and total number of points 
+-- Information Need: Customers are able to purchase more than one item per order
+-- SQL Query to Demonstrate Need:
+    -- For all orders with more than 1 item purchase (ex: purchasing coffee AND cake), return the order id, and number of items in the order. 
+SELECT order_id, COUNT(DISTINCT product_id) AS NumberOfItems
+FROM orders
+GROUP BY order_id
+HAVING COUNT(DISTINCT product_id) > 1;
+
+
+-- Information Need: Each customer can create an account associated with their phone number to earn rewards with future purchases
+-- SQL Query to Demonstrate Need:
+    -- For all customers who have rewards accounts with more than 10 points, return their first name, account number, and total number of points 
 SELECT firstname, account_num, total_points
 FROM rewards_account
 WHERE total_points > 10;
 
 
--- For customer JOHN DOE, output all their previous orders from Jan 1st, 2023 to Jan 8th 2023. Return the order date, total amount spent on order, a running total that aggregates sum of each order. Order by order date. 
-SELECT order_date, order_total, SUM(order_total) OVER (order by order_id) as RunningTotalPoint
+-- Information Need: Customers are able to access all their previous order histories through their card number OR phone number if they have a rewards account. 
+-- SQL Query to Demonstrate Need:
+    -- For customer with phone number (111)-111-1111, output all their previous orders from Jan 1st, 2023 to Jan 8th 2023. Return the order date, total amount spent on order. Order by order date. 
+SELECT order_date, order_total
 FROM orderHistory 
-JOIN customers ON customers.id = orderHistory.cid
-WHERE (customer.firstname = "JOHN" and customer.lastname = "DOE") AND ('01-01-2023' <= order_date <= '01-08-2023')
+JOIN rewards_account ON rewards_account.cid = orderHistory.cid
+WHERE (rewards_account.phone_number = '(111)-111-1111') AND ('01-01-2023' <= order_date <= '01-08-2023')
 ORDER BY order_date;
 
 
--- Find how many times customer JOHN DOE has placed an order that contains any form of ice cream as one of the order items. Note this can apply to any order placed in the year 2023. 
-SELECT COUNT(DISTINCT o.order_id) AS TotalIceCreamOrders
+-- Information Need: Customers are able to have a running total of points earned till date with the purchases they've made. This only applies to customers with rewards accounts. 
+-- SQL Query to Demonstrate Need:
+    -- Return the firstname, lastname, account number, and running total
+SELECT c.firstname, c.lastname, ra.account_num, SUM(o.points_earned) OVER (o.id) AS RunningTotalPoints
 FROM orders o
 JOIN customers c ON o.cid = c.id
-JOIN products p ON o.order_id = p.order_id
-WHERE (c.firstname = 'JOHN' AND c.lastname = 'DOE')
-        AND (p.item_name LIKE '%ice cream%')
-            AND YEAR(o.order_date) = 2023;
-
-
--- Find the total amount spent on ice-cream, cake, and coffee orders by customers in 2023. Output the item name along with total spent. Order by total spent from highest to lowest. 
-SELECT oi.item_name AS Category, SUM(o.order_total) AS TotalSpent
-FROM orders o
-JOIN products p ON o.order_id = p.order_id
-JOIN customers c ON o.cid = c.id
-WHERE (p.item_name LIKE '%ice cream%' 
-        OR p.item_name LIKE '%cake%' 
-            OR p.item_name LIKE '%coffee%')
-                AND YEAR(o.order_date) = 2023
-GROUP BY p.item_name
-ORDER BY TotalSpent DESC;
-
+JOIN rewards_account ra ON c.id = ra.customer_id
+ORDER BY RunningTotalPoints;
 
 
 
